e15d12e6da91312940ca5d745dba40ad851fb2b5
mmslama <mikolas.slama01@gmail.com>
2020-04-24 14:25:22 -0500

    My server 1.0.0

--- projects/ChatAway/Server/index.js
+++ projects/ChatAway/Server/index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const router = require('./routers/router');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 
 const app = express();
-
-app.use(router);
-app.listen(PORT, () => console.log(`Server runnin on ${PORT}...`));
-
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(cors());
+app.use(router);
 
 io.on('connection', (socket) =>{
 	console.log('New connection!');
@@ -26,4 +26,4 @@ io.on('connection', (socket) =>{
 });
 
 
-
+app.listen(PORT, () => console.log(`Server runnin on ${PORT}...`));
